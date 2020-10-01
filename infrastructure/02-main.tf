8f4729cdc966421aa3c0d03b55d14600a16fadda
Anton Bagaiev <abagayev@gmail.com>
2020-10-01 22:16:58 +0300

    Added lambda environment variables and schedule

--- infrastructure/02-main.tf
+++ infrastructure/02-main.tf
@@ -8,6 +8,22 @@ data "template_file" "lambda_policy_template" {
   template = "${file(format("%s/templates/lambda_policy_template", path.module))}"
 }
 
+data "local_file" "twitter_access_token" {
+  filename = "secrets/twitter_access_token.txt"
+}
+
+data "local_file" "twitter_access_secret" {
+  filename = "secrets/twitter_access_secret.txt"
+}
+
+data "local_file" "twitter_consumer_key" {
+  filename = "secrets/twitter_consumer_key.txt"
+}
+
+data "local_file" "twitter_consumer_secret" {
+  filename = "secrets/twitter_consumer_secret.txt"
+}
+
 data "archive_file" "lambda_zip" {
   type = "zip"
   source_file = "../src/lambda_function.py"
@@ -56,11 +72,36 @@ resource "aws_lambda_function" "lambda" {
 
   environment {
     variables = {
-      // @todo
+      TWITTER_CONSUMER_KEY = "${chomp(data.local_file.twitter_consumer_key.content)}",
+      TWITTER_CONSUMER_SECRET = "${chomp(data.local_file.twitter_consumer_secret.content)}",
+      TWITTER_ACCESS_TOKEN = "${chomp(data.local_file.twitter_access_token.content)}",
+      TWITTER_ACCESS_SECRET = "${chomp(data.local_file.twitter_access_secret.content)}",
     }
   }
 
   layers = [
     "${aws_lambda_layer_version.packages_layer.arn}"
   ]
+}
+
+# Cloudwatch schedule
+
+resource "aws_cloudwatch_event_rule" "lambda_event_rule" {
+  name = "${var.lambda_handler}-event-rule"
+  description = "${var.lambda_name} event rule"
+  schedule_expression = "${var.lambda_schedule}"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_event_target" {
+  rule = "${aws_cloudwatch_event_rule.lambda_event_rule.name}"
+  target_id = "lambda"
+  arn = "${aws_lambda_function.lambda.arn}"
+}
+
+resource "aws_lambda_permission" "lambda_cloudwatch_permission" {
+  statement_id = "AllowExecutionFromCloudWatch"
+  action = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.lambda.function_name}"
+  principal = "events.amazonaws.com"
+  source_arn = "${aws_cloudwatch_event_rule.lambda_event_rule.arn}"
 }
--- infrastructure/terraform.tfvars
+++ infrastructure/terraform.tfvars
@@ -1,3 +1,5 @@
 lambda_name = "lambda-phpdaily"
 lambda_handler = "lambda_function.lambda_handler"
 lambda_timeout = 60
+
+lambda_schedule = "rate(23 hours)"
