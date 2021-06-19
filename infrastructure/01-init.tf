bb36d019570691f2d576370c9d1ecb6ade66ec60
Anton Bagaiev <abagayev@gmail.com>
2021-06-19 21:55:47 +0300

    Added terraform cloud remote backend

--- infrastructure/01-init.tf
+++ infrastructure/01-init.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region  = "eu-central-1"
   profile = "dontgiveafish"
 }
+
+terraform {
+  backend "remote" {
+    organization = "dontgiveafish"
+
+    workspaces {
+      name = "phpdaily_"
+    }
+  }
+}
