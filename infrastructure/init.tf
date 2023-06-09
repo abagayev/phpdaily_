a6c03c2866a19864cbfdd2b64be61dc157cbcd6f
Anton Bagaiev <abagayev@gmail.com>
2023-06-09 15:57:21 +0200

    Bumped AWS provider version

--- infrastructure/init.tf
+++ infrastructure/init.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region  = "eu-central-1"
   profile = "dontgiveafish"
