12de3f4d632c15865f21b9bbfa54af4699925d61
Anton Bagaiev <abagayev@gmail.com>
2023-06-09 12:42:55 +0200

    Update tweepy to use Twitter API v2

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -76,20 +76,12 @@ def describe_package():
 
 
 def post_tweet(tweet):
-    consumer_key = os.environ['TWITTER_CONSUMER_KEY']
-    consumer_secret = os.environ['TWITTER_CONSUMER_SECRET']
-    access_token = os.environ['TWITTER_ACCESS_TOKEN']
-    access_secret = os.environ['TWITTER_ACCESS_SECRET']
+    client = tweepy.Client(consumer_key=os.environ['TWITTER_CONSUMER_KEY'],
+                           consumer_secret=os.environ['TWITTER_CONSUMER_SECRET'],
+                           access_token=os.environ['TWITTER_ACCESS_TOKEN'],
+                           access_token_secret=os.environ['TWITTER_ACCESS_SECRET'])
 
-    # authorize twitter
-
-    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-    auth.set_access_token(access_token, access_secret)
-    api = tweepy.API(auth)
-
-    # update status
-
-    api.update_status(status=tweet)
+    client.create_tweet(tweet)
 
 
 def lambda_handler(event, context):
